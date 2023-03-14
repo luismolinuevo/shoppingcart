544fb0d8dbabc872457d68d2d5b94780ff8b2735
luismolinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-03-13 20:16:20 -0400

    create basic layout of the nav bar

--- react/src/componets/Navbar.jsx
+++ react/src/componets/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+        <h1 className='logo'>Wamazon</h1>
+        <div>
+            <input type='search' placeholder='Search for products'/>
+            <Link to="/shoppingcart">Cart</Link>
+        </div>
+    </div>
   )
 }
