853cd3c3333686afa27cc39caea61a7d99cc3c67
luismolinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-03-14 15:58:22 -0400

    added flex box

--- react/src/pages/LandingPage/LandingPage.jsx
+++ react/src/pages/LandingPage/LandingPage.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import products from '../../products'
+import "./LandingPage.css"
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className='cardContainer'>
       {
         products.map((item) => (
           <div className='card'>
             <img src={item.image}/>
             <h1>{item.name}</h1>
+            <p>{item.category}</p>
+            <p>{item.price}</p>
+            <button>Add to Cart</button>
           </div>
         ))
       }
     </div>
   )
 }
+
