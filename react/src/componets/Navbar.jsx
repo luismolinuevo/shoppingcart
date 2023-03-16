e65abe5df42c39917b67ac53f3474dbf2bfc81d9
luismolinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-03-16 17:01:12 -0400

    created basic styles for navbar, made responsive

--- react/src/componets/Navbar.jsx
+++ react/src/componets/Navbar.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {FiShoppingCart} from "react-icons/fi"
 
 
 export default function Navbar() {
+  const totalCost = useSelector(state => state.cart.totalCost);
   return (
-    <div className='container'>
-      <div className='header'>
-        <h1 className='logo'>Wamazon</h1>
-        <div>
-          <input type='search' placeholder='Search for products' className='searchBar' />
-          <Link to="/cart">Cart</Link>
+    <header className='bg-headercolor'>
+    <div className=" px-10 sm:px-15">
+      <div className="flex justify-center sm:justify-between h-16 items-center">
+        <Link to="/" className=" hidden sm:flex text-4xl text-white">Wamazon</Link>
+        <div className='flex'>
+          <input type='search' placeholder='Search for products' className="w-60 h-8 sm:w-80 sm:h-10" />
+          <div className='flex items-center text-white w-1/6 pl-4' >
+            <Link to="/cart"><FiShoppingCart className="text-2xl"/></Link>
+            <p className="pl-1">{totalCost}</p>
+          </div>
+          
         </div>
       </div>
     </div>
+    </header>
   )
 }
